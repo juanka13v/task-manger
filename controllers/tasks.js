c07cd8a3f6fba837e5837ff925f90e54523ce8ee
Juan Valero <valerojuan13c@gmail.com>
2021-10-26 21:28:09 -0300

    primer commit

---
+++ controllers/tasks.js
@@ -0,0 +1,29 @@
+
+
+const getAllTasks = (req, res) => {
+    res.send('all items of the file');
+}
+
+const getTask = (req, res) => {
+    res.json({id:req.params.id});
+};
+
+const createTask = (req, res) => {
+    res.json(req.body);
+};
+
+const updateTask = (req, res) => {
+    res.send('update task');
+};
+
+const deleteTask = (req, res) => {
+    res.send('delete task');
+};
+
+module.exports = {
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
+}
